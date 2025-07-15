57fc624c535bd67dd2ad02808961cc633677ddf6
narakushi <taina.santana1305@gmail.com>
2025-07-15 09:24:17 -0300

    :sparkles: feat: adicionando form wrapper

--- src/layout/stepper/index.jsx
+++ src/layout/stepper/index.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react";
 import styles from "./index.module.css";
 import { usePathname } from "next/navigation";
 
-export const Stepper = ({ step, arrow, stepTwo }) => {
+export const Stepper = ({ currentStep, numberOfSteps }) => {
   const pathname = usePathname();
 
   return (
     <div className={styles.stepper}>
       <ul className={styles.child}>
-        <li>
-          <span className={styles.stepOrder}>{step.order}</span>
-          <span>{step.description}</span>
-        </li>
-
-        <li>{arrow}</li>
-
-        <li>
-          <span className={styles.stepOrder}>{stepTwo?.order}</span>
-          <span>{stepTwo?.description}</span>
-        </li>
+        <li>oi</li>
       </ul>
     </div>
   );
--- src/pages/registerOrg/index.jsx
+++ src/pages/registerOrg/index.jsx
@@ -2,48 +2,23 @@ import { Container } from "@/layout/ui/container";
 import styles from "./index.module.css";
 import { Title } from "@/layout/ui/title";
 import { useContext, useEffect, useState } from "react";
-import { changeKey } from "@/utils/changeKey";
-import { FormBase } from "@/layout/formBase";
-import inputs from "@/../public/jsons/inputsOrg.json";
-import { Button } from "@/layout/ui/button";
-import Link from "next/link";
 import { FormContext } from "@/context/FormContext";
 import { Stepper } from "@/layout/stepper";
-import { IoIosArrowForward } from "react-icons/io";
+import { Input } from "@/layout/ui/input";
+import { Form } from "@/layout/ui/form";
 
 export default function RegisterOrg() {
   const { formOrg, setFormOrg } = useContext(FormContext);
-
-  const [steps, setSteps] = useState({
-    step: { order: 1, description: "Dados da Organização" },
-    arrow: <IoIosArrowForward size={18} />,
-  });
-
-  useEffect(() => {
-    if (formOrg["Perfil_Atuacao"] === "Empresa Ofertante") {
-      setSteps({
-        ...steps,
-        stepTwo: { order: 2, description: "Solução ou serviço ofertado" },
-      });
-    } else {
-      setSteps({
-        ...steps,
-        stepTwo: { order: 2, description: "Necessidade ou desafio" },
-      });
-    }
-  }, [formOrg["Perfil_Atuacao"]]);
 
   return (
     <div className={styles.containerRegisterOrg}>
       <Container>
         <Title text="Cadastro da organização" />
         <p>Abaixo informe os dados da organização que deseja cadastrar</p>
-        <Stepper {...steps} />
-        <FormBase inputs={inputs} formData={formOrg} setFormData={setFormOrg}>
-          <Button customClass="btnColor">
-            <Link href="/">Continuar</Link>
-          </Button>
-        </FormBase>
+        <Stepper />
+        <Form>
+          <input type="text" />
+        </Form>
       </Container>
     </div>
   );
